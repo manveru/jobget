9316f18205636b41799e405746d9876c41bf919b
Michael Fellinger <m.fellinger@gmail.com>
2008-08-12 20:42:16 +0900

    Update specs

--- spec/recruiter-post-job.rb
+++ spec/recruiter-post-job.rb
@@ -1,6 +1,6 @@
 require 'spec/helper'
 
-describe 'Recruiter comes to site and wants to post a job' do
+describe 'Recruiter signs up and posts a job' do
   behaves_like 'mechanize'
 
   name = Faker::Name::name
@@ -61,26 +61,13 @@ describe 'Recruiter comes to site and wants to post a job' do
 
     page.at(:h2).inner_text.should == 'Ruby Developer'
   end
-end
 
-# [2008-08-11 18:17:10] INFO   Dynamic request from 127.0.0.1: /
-# [2008-08-11 18:17:11] INFO   Dynamic request from 127.0.0.1: /user/join
-# [2008-08-11 18:17:13] INFO   Dynamic request from 127.0.0.1: /user/join?role=recruiter
-# [2008-08-11 18:17:23] INFO   Dynamic request from 127.0.0.1: /user/join?role=recruiter
-# [2008-08-11 18:17:25] INFO   Dynamic request from 127.0.0.1: /user/join?role=recruiter
-# [2008-08-11 18:17:35] INFO   Dynamic request from 127.0.0.1: /user/join?role=recruiter
-# [2008-08-11 18:17:35] INFO   Redirect to 'http://localhost:7000/user/read'
-# [2008-08-11 18:17:35] INFO   Dynamic request from 127.0.0.1: /user/read
-# [2008-08-11 18:17:38] INFO   Dynamic request from 127.0.0.1: /company/edit
-# [2008-08-11 18:17:52] INFO   Dynamic request from 127.0.0.1: /company/edit
-# [2008-08-11 18:17:54] INFO   Redirect to 'http://localhost:7000/company/edit'
-# [2008-08-11 18:17:54] INFO   Dynamic request from 127.0.0.1: /company/edit
-# [2008-08-11 18:17:55] INFO   Dynamic request from 127.0.0.1: /job/manage
-# [2008-08-11 18:17:57] INFO   Dynamic request from 127.0.0.1: /job/post
-# [2008-08-11 18:18:11] INFO   Dynamic request from 127.0.0.1: /job/post
-# [2008-08-11 18:18:11] INFO   Redirect to 'http://localhost:7000/job/read/1-Some-job'
-# [2008-08-11 18:18:11] INFO   Dynamic request from 127.0.0.1: /job/read/1-Some-job
-# [2008-08-11 18:18:14] INFO   Dynamic request from 127.0.0.1: /user/read
-# [2008-08-11 18:18:15] INFO   Dynamic request from 127.0.0.1: /application
-# [2008-08-11 18:18:15] INFO   Dynamic request from 127.0.0.1: /job/browse
-# [2008-08-11 18:18:17] INFO   Dynamic request from 127.0.0.1: /
+  should 'head to main page and see job listed as latest' do
+    click page.links.first
+    (page/:h2).map{|h2| h2.inner_text }.should == %w[Featured Latest]
+
+    link = page.at('.latest/.job/a')
+    link[:href].should == "/job/read/1-Ruby-Developer"
+    link.inner_text.should == 'Ruby Developer'
+  end
+end
